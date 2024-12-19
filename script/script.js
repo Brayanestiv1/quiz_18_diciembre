51984dbb04d30b0a3380609d6e5713e7f205f6ed
Brayanestiv1 <cuentaestudio1504@gmail.com>
2024-12-18 20:44:23 -0500

    commit entrega

---
+++ script/script.js
@@ -0,0 +1,20 @@
+/*
+    debo llamar a el alias numero uno
+    debo llamar al alias numero dos
+    debo llamar el id del boton de fait
+    debo hacer una funcion, que al dar click en el button primero, compruebe si estan los dos alias, si es asi, va a generar un tres por tres en la pantalla
+/*
+*/ 
+
+const alias1 = documentgetElementById('alias1');
+const alias2 = documentgetElementById('alias2');
+const fait1 = documentgetElementById('fait');
+
+let turnos = "";
+
+juego.addEventListener("click", (fait) => {
+    
+})
+
+document.getElementById("alias1");
+document.getElementById("alias2");
